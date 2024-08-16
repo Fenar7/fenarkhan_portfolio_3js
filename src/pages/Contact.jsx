af983c8920d72b84125bf8de5534ae9d417f7f79
Fenar Khan <fenarkhan@Mac-Pro.local>
2024-08-16 23:31:49 +0530

    jskdjfsldf:

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,101 +1,143 @@
-import React from 'react'
-import { useState } from 'react'
-import { useRef } from 'react'
+import React, { useState, useRef, Suspense } from 'react';
+import emailjs from '@emailjs/browser';
+import Fox from '../models/Fox';
+import { Canvas } from '@react-three/fiber';
+import Loader from '../components/Loader';
 
 const Contact = () => {
-  const formRef = useRef(null)
-  const [form,setForm] = useState({
+  const formRef = useRef(null);
+  const [form, setForm] = useState({
     name: '',
     email: '',
     message: '',
-  })
-  const[isLoading,setIsLoading] = useState(false)
+  });
+  const [isLoading, setIsLoading] = useState(false);
+  const [currentAnimation,setCurrentAnimation] = useState('idle')
 
-  const handleChange = () =>{
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    setCurrentAnimation('run')
+
+    emailjs.send(
+      import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+      import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+      {
+        from_name: form.name,
+        la_name: "Fenar",
+        from_email: form.email,
+        to_email: 'fenarkhan7@gmail.com',
+        message: form.message,
+      },
+      import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+    ).then(() => {
+      setIsLoading(false);
+      setForm({ name: '', email: '', message: '' });
+    }).catch((error) => {
+      setIsLoading(false);
+      setCurrentAnimation('idle')
+      console.log(error);
+    });
+  };
+
+  const handleFocus = () =>{
+    setCurrentAnimation('walk')
   }
 
-  const onFocus = () =>{
-
-  }
-
-  const onBlur = () =>{
-
-  }
-
-  const handleSubmit = () =>{
-
+  const handleBlur = () =>{
+    setCurrentAnimation('idle')
   }
 
   return (
-    <section className='relative flex lg:flex-row
-    flex-col max-container'>
+    <section className='relative flex lg:flex-row flex-col max-container'>
       <div className='flex-1 min-w-[50%] flex flex-col'>
-        <h1 className='head-text'>
-          Get in Touch
-        </h1>
+        <h1 className='head-text'>Get in Touch</h1>
 
         <form
-        onSubmit={handleSubmit}
-        className="w-full flex flex-col gap-7
-        mt-14">
+          onSubmit={handleSubmit}
+          className="w-full flex flex-col gap-7 mt-14"
+        >
           <label htmlFor="" className='text-black-500 font-semibold'>
-          Name
-          <input
-          type='text'
-          name='name'
-          className='input'
-          placeholder='Jhon'
-          required
-          value={form.name}
-          onChange={handleChange}
-          onFocus={onFocus}
-          onBlur={onBlur}
-          />
+            Name
+            <input
+              type='text'
+              name='name'
+              className='input'
+              placeholder='John'
+              required
+              value={form.name}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
           </label>
 
           <label htmlFor="" className='text-black-500 font-semibold'>
-          Email
-          <input
-          type='text'
-          name='email'
-          className='input'
-          placeholder='jhon@gmail.com'
-          required
-          value={form.email}
-          onChange={handleChange}
-          onFocus={onFocus}
-          onBlur={onBlur}
-          />
+            Email
+            <input
+              type='text'
+              name='email'
+              className='input'
+              placeholder='john@gmail.com'
+              required
+              value={form.email}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
           </label>
 
           <label htmlFor="" className='text-black-500 font-semibold'>
-          Your Message
-          <textarea
-          name='email'
-          className='textarea'
-          placeholder='Let me know how I can help you'
-          required
-          value={form.message}
-          rows={4}
-          onChange={handleChange}
-          onFocus={onFocus}
-          onBlur={onBlur}
-          />
+            Your Message
+            <textarea
+              name='message'
+              className='textarea'
+              placeholder='Let me know how I can help you'
+              required
+              value={form.message}
+              rows={4}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
           </label>
 
-          <button type='submit'
-          className='btn'
-          disabled={isLoading}
-          onFocus={onFocus}
-          onBlur={onBlur}
+          <button type='submit' className='btn' disabled={isLoading}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
           >
             {isLoading ? 'Sending...' : 'Send Message'}
           </button>
         </form>
       </div>
-    </section>
-  )
-}
 
-export default Contact
+      <div className='lg:w-1/2 w-full lg:h-auto md:h-[550px] h-[350px]'>
+        <Canvas
+        camera={{
+          position: [0, 0, 5],
+          fov: 75,
+          near: 0.1,
+          far: 1000
+         }}
+        >
+          <directionalLight intensity={2.5} position={[0,0,1]}/>
+          <ambientLight intensity={0.5}/>
+          <Suspense fallback={<Loader />}>
+            <Fox
+            currentAnimation={currentAnimation}
+              position={[0.5, 0.35, 0]}
+              rotation={[12.6, -0.6, 0]}
+              scale={[0.5, 0.5, 0.5]}
+            />
+          </Suspense>
+        </Canvas>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
