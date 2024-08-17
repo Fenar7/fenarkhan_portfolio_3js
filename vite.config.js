af6f8006da83c08540c4529e434645401cead0da
Fenar Khan <fenarkhan@Mac-Pro.local>
2024-08-18 02:00:37 +0530

    1223

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.glb'],
-  resolve: {
-    alias: {
-      '@emailjs/browser': require.resolve('@emailjs/browser')
-    }
-  }
+  assetsInclude: ['**/*.glb']
 })
