5a78e144a224f42ef55b7769f42094d95e2955b0
Fenar Khan <fenarkhan@Mac-Pro.local>
2024-08-18 01:43:19 +0530

    higgdfgdg

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.glb'],
-  build: {
-    rollupOptions: {
-      external: ['@emailjs/browser'],
+  resolve: {
+    alias: {
+      '@emailjs/browser': '/node_modules/@emailjs/browser/dist/email.min.js',
     },
-  }
+  },
 })
